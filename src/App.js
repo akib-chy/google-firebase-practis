54b6dcc475717e0a52cc3551633a2b9cf47b3e93
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-04-08 12:23:26 +0600

    google firebase signout btn added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import "./App.css";
 import app from "./firebase.init";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 import { useState } from "react";
 
 const auth = getAuth(app);
@@ -18,10 +23,25 @@ function App() {
         console.error(error);
       });
   };
+  const handleGoogleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({});
+        console.log("successfull sign out");
+      })
+      .catch((error) => {
+        setUser({});
+        console.error(error);
+      });
+  };
   return (
     <div className="App">
       <h1>Welcome To React</h1>
-      <button onClick={handleGoogleSignIn}>google sign in</button>
+      {user.email ? (
+        <button onClick={handleGoogleSignOut}>Sign Out</button>
+      ) : (
+        <button onClick={handleGoogleSignIn}>google sign in</button>
+      )}
       <h3>{user.displayName}</h3>
       <p>{user.email}</p>
       <img src={user.photoURL} alt="" />
