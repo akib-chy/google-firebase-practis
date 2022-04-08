d8b4897651930b80c5fecac54e3d61fc9fef5788
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-04-08 12:14:50 +0600

    show user data on display

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import "./App.css";
 import app from "./firebase.init";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useState } from "react";
 
 const auth = getAuth(app);
 function App() {
+  const [user, setUser] = useState({});
   const provider = new GoogleAuthProvider();
   const handleGoogleSignIn = () => {
     signInWithPopup(auth, provider)
       .then((result) => {
         const user = result.user;
+        setUser(user);
         console.log(user);
       })
       .catch((error) => {
@@ -19,6 +22,9 @@ function App() {
     <div className="App">
       <h1>Welcome To React</h1>
       <button onClick={handleGoogleSignIn}>google sign in</button>
+      <h3>{user.displayName}</h3>
+      <p>{user.email}</p>
+      <img src={user.photoURL} alt="" />
     </div>
   );
 }
